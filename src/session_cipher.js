41aa0f8c3dff94e9ccd2937f3c198137d6f98f89
Hauze. <neirmossoftware@gmail.com>
2025-06-13 16:27:56 -0500

    Update session_cipher.js

--- src/session_cipher.js
+++ src/session_cipher.js
@@ -169,14 +169,15 @@ class SessionCipher {
             const remoteIdentityKey = result.session.indexInfo.remoteIdentityKey;
             if (!await this.storage.isTrustedIdentity(this.addr.id, remoteIdentityKey)) {
                 throw new errors.UntrustedIdentityKeyError(this.addr.id, remoteIdentityKey);
-            }   
+            }
+            console.log(result)
             if (record.isClosed(result.session)) {
                 // It's possible for this to happen when processing a backlog of messages.
                 // The message was, hopefully, just sent back in a time when this session
                 // was the most current.  Simply make a note of it and continue.  If our
                 // actual open session is for reason invalid, that must be handled via
                 // a full SessionError response.
-                console.warn("Decrypted message with closed session.");
+                // console.warn("Decrypted message with closed session.");
             }
             await this.storeRecord(record);
             return result.plaintext;
