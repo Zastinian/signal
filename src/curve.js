d9b9fc10e390121570eeab4c57f0f3608b07e67e
Zastinian <zastinian@gmail.com>
2025-07-08 20:07:09 -0600

    test: noble curves

--- src/curve.js
+++ src/curve.js
@@ -1,105 +1,52 @@
 "use strict";
-const nacl = require("tweetnacl");
+
+const { x25519 } = require("@noble/curves/ed25519");
+const nodeCrypto = require("crypto");
 
 function validatePrivKey(privKey) {
-  if (privKey === undefined) {
-    throw new Error("Undefined private key");
-  }
-  if (!(privKey instanceof Buffer)) {
-    throw new Error(`Invalid private key type: ${privKey?.constructor?.name}`);
-  }
-  if (privKey.byteLength != 32) {
-    throw new Error(`Incorrect private key length: ${privKey?.byteLength}`);
-  }
+  if (privKey === undefined) throw new Error("Undefined private key");
+  if (!(privKey instanceof Buffer)) throw new Error(`Invalid private key type: ${privKey?.constructor?.name}`);
+  if (privKey.byteLength !== 32) throw new Error(`Incorrect private key length: ${privKey?.byteLength}`);
 }
 
 function scrubPubKeyFormat(pubKey) {
-  if (!(pubKey instanceof Buffer)) {
-    throw new Error(`Invalid public key type: ${pubKey?.constructor?.name}`);
-  }
-  if (
-    pubKey === undefined ||
-    ((pubKey.byteLength != 33 || pubKey[0] != 5) && pubKey.byteLength != 32)
-  ) {
+  if (!(pubKey instanceof Buffer)) throw new Error(`Invalid public key type: ${pubKey?.constructor?.name}`);
+  if (pubKey === undefined || ((pubKey.byteLength !== 33 || pubKey[0] !== 5) && pubKey.byteLength !== 32))
     throw new Error("Invalid public key");
-  }
-  if (pubKey.byteLength == 33) {
-    return pubKey.subarray(1);
-  } else {
-    console.error(
-      "WARNING: Expected pubkey of length 33, please report the ST and client that generated the pubkey",
-    );
+  if (pubKey.byteLength === 33) return pubKey.subarray(1);
+  else {
+    console.error("WARNING: Expected pubkey of length 33, please report the ST and client that generated the pubkey");
     return pubKey;
   }
 }
 
 exports.createKeyPair = function (privKey) {
   validatePrivKey(privKey);
-
-  const boxKeyPair = nacl.box.keyPair.fromSecretKey(new Uint8Array(privKey));
-
   const pub = new Uint8Array(33);
-  pub.set(boxKeyPair.publicKey, 1);
+  pub.set(x25519.getPublicKey(privKey), 1);
   pub[0] = 5;
-
   return {
     pubKey: Buffer.from(pub),
-    privKey: Buffer.from(boxKeyPair.secretKey),
+    privKey: Buffer.from(privKey),
   };
 };
 
 exports.calculateAgreement = function (pubKey, privKey) {
-  let scrubbedPubKey = scrubPubKeyFormat(pubKey);
+  const scrubbedPubKey = scrubPubKeyFormat(pubKey);
   validatePrivKey(privKey);
-
-  if (!scrubbedPubKey || scrubbedPubKey.byteLength != 32) {
-    throw new Error("Invalid public key");
-  }
-
-  const shared = nacl.box.before(new Uint8Array(scrubbedPubKey), new Uint8Array(privKey));
-  return Buffer.from(shared);
+  if (!scrubbedPubKey || scrubbedPubKey.byteLength !== 32) throw new Error("Invalid public key");
+  return Buffer.from(x25519.sharedKey(privKey, scrubbedPubKey));
 };
 
-exports.calculateSignature = function (privKey, message) {
-  validatePrivKey(privKey);
-
-  if (!message) {
-    throw new Error("Invalid message");
-  }
-
-  const signKeyPair = nacl.sign.keyPair.fromSeed(new Uint8Array(privKey));
-  const signature = nacl.sign.detached(new Uint8Array(message), signKeyPair.secretKey);
-
-  return Buffer.from(signature);
+exports.calculateSignature = function () {
+  throw new Error("Signature not supported with pure x25519");
 };
 
-exports.verifySignature = function (pubKey, msg, sig, isInit) {
-  let scrubbedPubKey = scrubPubKeyFormat(pubKey);
-
-  if (!scrubbedPubKey || scrubbedPubKey.byteLength != 32) {
-    throw new Error("Invalid public key");
-  }
-
-  if (!msg) {
-    throw new Error("Invalid message");
-  }
-
-  if (!sig || sig.byteLength != 64) {
-    throw new Error("Invalid signature");
-  }
-
-  if (isInit) {
-    return true;
-  }
-
-  return nacl.sign.detached.verify(
-    new Uint8Array(msg),
-    new Uint8Array(sig),
-    new Uint8Array(scrubbedPubKey),
-  );
+exports.verifySignature = function () {
+  throw new Error("Signature verification not supported with pure x25519");
 };
 
 exports.generateKeyPair = function () {
-  const privKey = nacl.randomBytes(32);
-  return exports.createKeyPair(Buffer.from(privKey));
+  const privKey = nodeCrypto.randomBytes(32);
+  return exports.createKeyPair(privKey);
 };
